5a7eba7070782dee657df94054226c00d14c92e7
Sai Kiran <ganeshsaikiran7@gmail.com>
2023-07-03 16:07:13 +0530

    Add read more button, individual blog page, fix footer, and minor bug fixes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
+const _ = require("lodash")
 
 const app = express()
 
@@ -8,8 +9,8 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
-const titles = [] //, "Day 1", "Day 2" "Home"
-const userJournal = []//,"This is the home page. You can write your journals." "This is the home page. You can write your journals.", "This is the home page. You can write your journals."
+const titles = ["Day1"] //, , "Day 2" "Home"
+const userJournal = ["This is the home page. You can write your journals.This is the home page. You can write your journals.This is the home page. You can write your journals."]//,
 let len = titles.length
 app.get("/", function (req, res) {
     res.render('list', { headings: titles, journals: userJournal, num: len })
@@ -27,10 +28,18 @@ app.get("/contact", function (req, res) {
     res.render('contact')
 })
 
+
+app.get("/:heading", function (req, res) {
+    let requestedTitle = req.params.heading
+    if (titles.includes(requestedTitle)) {
+        let indexofElement = titles.indexOf(requestedTitle)
+        res.render("post", { heading: requestedTitle, journal: userJournal[indexofElement] })
+    }
+})
+
 app.post("/", function (req, res) {
     let journalHeading = String(req.body.cheading)
     let journalText = String(req.body.cjournal)
-    console.log(journalHeading, journalText)
     titles.push(journalHeading)
     userJournal.push(journalText)
     res.redirect("/")
